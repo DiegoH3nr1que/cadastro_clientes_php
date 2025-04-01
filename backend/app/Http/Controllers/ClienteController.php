416c222188b4d448d7c4ab47e8ea7a77a284d190
Diego <diegohenrique123012@gmail.com>
2025-03-31 23:07:40 -0300

    feat(backend\app\Http\Controllers): criação do controller para as duas rotas exigidas no projeto (GET/POST)

---
+++ backend/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Cliente::all());
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nome' => 'required|string',
+            'email' => 'required|email|unique:clientes',
+        ]);
+
+        $data['data_cadastro'] = now();
+
+        $cliente = Cliente::create($data);
+
+        return response()->json($cliente, 201);
+    }
+}
