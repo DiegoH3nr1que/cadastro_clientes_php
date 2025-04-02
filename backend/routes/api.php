18da5fdaa8a940934122a1da460b62b8d3af12f2
Diego <diegohenrique123012@gmail.com>
2025-04-01 23:11:20 -0300

    feat: inserindo a rota de logs dentro do arquivo api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ApiExternalController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/clientes', [ClienteController::class, 'index']);
 Route::post('/clientes', [ClienteController::class, 'store']);
+Route::get('/logs', [ApiExternalController::class, 'index']);
